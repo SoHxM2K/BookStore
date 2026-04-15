c88a88d648f22c23e9bb88d36cc2b5af8927818a
soham <sohxm2k@gmail.com>
2026-04-16 02:50:04 +0530

    react componentts

---
+++ Frontend/src/pages/BookDetails.jsx
@@ -0,0 +1,8 @@
+export default function BookDetails() {
+  return (
+    <div>
+      <h1>Book Details</h1>
+      {/* Book details content goes here */}
+    </div>
+  );
+}
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function BookDetails() {
+  return (
+    <div>
+      <h1>Book Details</h1>
+      
+    </div>
+  );
+}
